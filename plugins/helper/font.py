87e42a6d6505820e5d3b7f94a67f4a8ba8097233
SHIV <shivashish4444@gmail.com>
2026-05-12 12:12:05 +0530

    Update font.py

--- plugins/helper/font.py
+++ plugins/helper/font.py
@@ -1,176 +1,201 @@
 import os
-from plugins.helper.fotnt_string import Fonts
+from plugins.helper.fotnt_string import Fonts  # Fix typo: fotnt_string → font_string
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from database.users_chats_db import db  # 🔥 PREMIUM CHECK
+import script
+from info import ADMINS
 
 @Client.on_message(filters.private & filters.command(["font"]))
-async def style_buttons(c, m, cb=False):
-    buttons = [[
-        InlineKeyboardButton('𝚃𝚢𝚙𝚎𝚠𝚛𝚒𝚝𝚎𝚛', callback_data='style+typewriter'),
-        InlineKeyboardButton('𝕆𝕦𝕥𝕝𝕚𝕟𝕖', callback_data='style+outline'),
-        InlineKeyboardButton('𝐒𝐞𝐫𝐢𝐟', callback_data='style+serif'),
-        ],[
-        InlineKeyboardButton('𝑺𝒆𝒓𝒊𝒇', callback_data='style+bold_cool'),
-        InlineKeyboardButton('𝑆𝑒𝑟𝑖𝑓', callback_data='style+cool'),
-        InlineKeyboardButton('Sᴍᴀʟʟ Cᴀᴘs', callback_data='style+small_cap'),
-        ],[
-        InlineKeyboardButton('𝓈𝒸𝓇𝒾𝓅𝓉', callback_data='style+script'),
-        InlineKeyboardButton('𝓼𝓬𝓻𝓲𝓹𝓽', callback_data='style+script_bolt'),
-        InlineKeyboardButton('ᵗⁱⁿʸ', callback_data='style+tiny'),
-        ],[
-        InlineKeyboardButton('ᑕOᗰIᑕ', callback_data='style+comic'),
-        InlineKeyboardButton('𝗦𝗮𝗻𝘀', callback_data='style+sans'),
-        InlineKeyboardButton('𝙎𝙖𝙣𝙨', callback_data='style+slant_sans'),
-        ],[
-        InlineKeyboardButton('𝘚𝘢𝘯𝘴', callback_data='style+slant'),
-        InlineKeyboardButton('𝖲𝖺𝗇𝗌', callback_data='style+sim'),
-        InlineKeyboardButton('Ⓒ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎', callback_data='style+circles')
-        ],[
-        InlineKeyboardButton('🅒︎🅘︎🅡︎🅒︎🅛︎🅔︎🅢︎', callback_data='style+circle_dark'),
-        InlineKeyboardButton('𝔊𝔬𝔱𝔥𝔦𝔠', callback_data='style+gothic'),
-        InlineKeyboardButton('𝕲𝖔𝖙𝖍𝖎𝖈', callback_data='style+gothic_bolt'),
-        ],[
-        InlineKeyboardButton('C͜͡l͜͡o͜͡u͜͡d͜͡s͜͡', callback_data='style+cloud'),
-        InlineKeyboardButton('H̆̈ă̈p̆̈p̆̈y̆̈', callback_data='style+happy'),
-        InlineKeyboardButton('S̑̈ȃ̈d̑̈', callback_data='style+sad'),
-        ],[
-        InlineKeyboardButton('Next ➡️', callback_data="nxt")
-    ]]
-    if not cb:
-        if ' ' in m.text:
-            title = m.text.split(" ", 1)[1]
-            await m.reply_text(title, reply_markup=InlineKeyboardMarkup(buttons), reply_to_message_id=m.id)                     
+async def font_handler(client: Client, message: Message):
+    """Font generator with premium features"""
+    user_id = message.from_user.id
+    is_premium = await db.is_premium_user(user_id)
+    
+    # Premium users get special fonts + no limits
+    if is_premium:
+        await message.reply_text(
+            "💎 **Premium User - Unlimited Fonts!**\n"
+            "Send: `/font your text`",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("✨ Premium Fonts", callback_data="premium_fonts")]
+            ])
+        )
+        return
+    
+    # Show font menu
+    buttons = await get_font_buttons()
+    if len(message.text.split()) > 1:
+        text = " ".join(message.text.split()[1:])
+        await message.reply_text(
+            text,
+            reply_markup=InlineKeyboardMarkup(buttons),
+            reply_to_message_id=message.id
+        )
+    else:
+        await message.reply_text(
+            script.FONT_TXT,
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+
+async def get_font_buttons():
+    """Font style buttons"""
+    return [
+        [
+            InlineKeyboardButton('𝚃𝚢𝚙𝚎𝚠𝚛𝚒𝚝𝚎𝚛', callback_data='style+typewriter'),
+            InlineKeyboardButton('𝕆𝕦𝕥𝕝𝕚𝕟𝕖', callback_data='style+outline'),
+            InlineKeyboardButton('𝐒𝐞𝐫𝐢𝐟', callback_data='style+serif'),
+        ], [
+            InlineKeyboardButton('𝑺𝒆𝒓𝒊𝒇', callback_data='style+bold_cool'),
+            InlineKeyboardButton('𝑆𝑒𝑟𝑖𝑓', callback_data='style+cool'),
+            InlineKeyboardButton('Sᴍᴀʟʟ Cᴀᴘs', callback_data='style+small_cap'),
+        ], [
+            InlineKeyboardButton('𝓈𝒸𝓇𝒾𝓅𝓉', callback_data='style+script'),
+            InlineKeyboardButton('𝓼𝓬𝓻𝓲𝓹𝓽', callback_data='style+script_bolt'),
+            InlineKeyboardButton('ᵗⁱⁿʸ', callback_data='style+tiny'),
+        ], [
+            InlineKeyboardButton('ᑕOᗰIᑕ', callback_data='style+comic'),
+            InlineKeyboardButton('𝗦𝗮𝗻𝘀', callback_data='style+sans'),
+            InlineKeyboardButton('𝙎𝙖𝙣𝙨', callback_data='style+slant_sans'),
+        ], [
+            InlineKeyboardButton("➡️ More", callback_data="nxt"),
+            InlineKeyboardButton("💎 Premium", callback_data="plan")
+        ]
+    ]
+
+@Client.on_callback_query(filters.regex('^(nxt|premium_fonts)$'))
+async def font_next(client: Client, callback: CallbackQuery):
+    """Next page fonts or premium fonts"""
+    if callback.data == "premium_fonts":
+        # Premium exclusive fonts
+        buttons = [
+            [InlineKeyboardButton('✨ G̶l̶i̶t̶c̶h̶', callback_data='style+glitch')],
+            [InlineKeyboardButton('🌈 R̷a̷i̷n̷b̷o̷w̷', callback_data='style+rainbow')],
+            [InlineKeyboardButton('🔥 F̸i̸r̸e̸', callback_data='style+fire')],
+            [InlineKeyboardButton("💎 /myplan", callback_data="myplan")],
+            [InlineKeyboardButton("🔙 Back", callback_data="font_back")]
+        ]
+        await callback.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+        return
+    
+    if callback.data == "nxt":
+        buttons = [
+            [InlineKeyboardButton('🇸 🇵 🇪 🇨 🇮 🇦 🇱 ', callback_data='style+special')],
+            [InlineKeyboardButton('🅂🅀🅄🄰🅁🄴🅂', callback_data='style+squares')],
+            [InlineKeyboardButton('🆂︎🆀︎🆄︎🅰︎🆁︎🅴︎🆂︎', callback_data='style+squares_bold')],
+            [InlineKeyboardButton('ꪖꪀᦔꪖꪶꪊᥴ𝓲ꪖ', callback_data='style+andalucia')],
+            [InlineKeyboardButton("🔙 Back", callback_data="font_back")]
+        ]
+        await callback.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+
+@Client.on_callback_query(filters.regex('^font_back'))
+async def font_back(client: Client, callback: CallbackQuery):
+    """Back to main fonts"""
+    buttons = await get_font_buttons()
+    await callback.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+
+@Client.on_callback_query(filters.regex('^(style|plan|myplan)'))
+async def font_style(client: Client, callback: CallbackQuery):
+    """Generate font style"""
+    if callback.data == "plan":
+        # Premium upsell
+        buttons = [
+            [InlineKeyboardButton("💎 1 Day - ₹2", callback_data="plan_1")],
+            [InlineKeyboardButton("💎 30 Days - ₹39", callback_data="plan_30")],
+            [InlineKeyboardButton("📞 Contact", url="https://t.me/betabot_hub")]
+        ]
+        await callback.edit_message_text(
+            script.PLAN_TEXT,
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+        return
+    
+    if callback.data == "myplan":
+        user_id = callback.from_user.id
+        premium_info = await db.get_premium_user(user_id)
+        if premium_info and premium_info.get("is_active"):
+            await callback.answer("💎 Premium Active!", show_alert=True)
         else:
-            await m.reply_text(text="Ente Any Text Eg:- `/font [text]`")    
-    else:
-        await m.answer()
-        await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
-
-
-@Client.on_callback_query(filters.regex('^nxt'))
-async def nxt(c, m):
-    if m.data == "nxt":
-        buttons = [[
-            InlineKeyboardButton('🇸 🇵 🇪 🇨 🇮 🇦 🇱 ', callback_data='style+special'),
-            InlineKeyboardButton('🅂🅀🅄🄰🅁🄴🅂', callback_data='style+squares'),
-            InlineKeyboardButton('🆂︎🆀︎🆄︎🅰︎🆁︎🅴︎🆂︎', callback_data='style+squares_bold'),
-            ],[
-            InlineKeyboardButton('ꪖꪀᦔꪖꪶꪊᥴ𝓲ꪖ', callback_data='style+andalucia'),
-            InlineKeyboardButton('爪卂几ᘜ卂', callback_data='style+manga'),
-            InlineKeyboardButton('S̾t̾i̾n̾k̾y̾', callback_data='style+stinky'),
-            ],[
-            InlineKeyboardButton('B̥ͦu̥ͦb̥ͦb̥ͦl̥ͦe̥ͦs̥ͦ', callback_data='style+bubbles'),
-            InlineKeyboardButton('U͟n͟d͟e͟r͟l͟i͟n͟e͟', callback_data='style+underline'),
-            InlineKeyboardButton('꒒ꍏꀷꌩꌃꀎꁅ', callback_data='style+ladybug'),
-            ],[
-            InlineKeyboardButton('R҉a҉y҉s҉', callback_data='style+rays'),
-            InlineKeyboardButton('B҈i҈r҈d҈s҈', callback_data='style+birds'),
-            InlineKeyboardButton('S̸l̸a̸s̸h̸', callback_data='style+slash'),
-            ],[
-            InlineKeyboardButton('s⃠t⃠o⃠p⃠', callback_data='style+stop'),
-            InlineKeyboardButton('S̺͆k̺͆y̺͆l̺͆i̺͆n̺͆e̺͆', callback_data='style+skyline'),
-            InlineKeyboardButton('A͎r͎r͎o͎w͎s͎', callback_data='style+arrows'),
-            ],[
-            InlineKeyboardButton('ዪሀክቿነ', callback_data='style+qvnes'),
-            InlineKeyboardButton('S̶t̶r̶i̶k̶e̶', callback_data='style+strike'),
-            InlineKeyboardButton('F༙r༙o༙z༙e༙n༙', callback_data='style+frozen')
-            ],[
-            InlineKeyboardButton('⬅️ Back', callback_data='nxt+0')
-        ]]
-        await m.answer()
-        await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
-    else:
-        await style_buttons(c, m, cb=True)
-
-
-@Client.on_callback_query(filters.regex('^style'))
-async def style(c, m):
-    await m.answer()
-    cmd, style = m.data.split('+')
-
-    if style == 'typewriter':
-        cls = Fonts.typewriter
-    if style == 'outline':
-        cls = Fonts.outline
-    if style == 'serif':
-        cls = Fonts.serief
-    if style == 'bold_cool':
-        cls = Fonts.bold_cool
-    if style == 'cool':
-        cls = Fonts.cool
-    if style == 'small_cap':
-        cls = Fonts.smallcap
-    if style == 'script':
-        cls = Fonts.script
-    if style == 'script_bolt':
-        cls = Fonts.bold_script
-    if style == 'tiny':
-        cls = Fonts.tiny
-    if style == 'comic':
-        cls = Fonts.comic
-    if style == 'sans':
-        cls = Fonts.san
-    if style == 'slant_sans':
-        cls = Fonts.slant_san
-    if style == 'slant':
-        cls = Fonts.slant
-    if style == 'sim':
-        cls = Fonts.sim
-    if style == 'circles':
-        cls = Fonts.circles
-    if style == 'circle_dark':
-        cls = Fonts.dark_circle
-    if style == 'gothic':
-        cls = Fonts.gothic
-    if style == 'gothic_bolt':
-        cls = Fonts.bold_gothic
-    if style == 'cloud':
-        cls = Fonts.cloud
-    if style == 'happy':
-        cls = Fonts.happy
-    if style == 'sad':
-        cls = Fonts.sad
-    if style == 'special':
-        cls = Fonts.special
-    if style == 'squares':
-        cls = Fonts.square
-    if style == 'squares_bold':
-        cls = Fonts.dark_square
-    if style == 'andalucia':
-        cls = Fonts.andalucia
-    if style == 'manga':
-        cls = Fonts.manga
-    if style == 'stinky':
-        cls = Fonts.stinky
-    if style == 'bubbles':
-        cls = Fonts.bubbles
-    if style == 'underline':
-        cls = Fonts.underline
-    if style == 'ladybug':
-        cls = Fonts.ladybug
-    if style == 'rays':
-        cls = Fonts.rays
-    if style == 'birds':
-        cls = Fonts.birds
-    if style == 'slash':
-        cls = Fonts.slash
-    if style == 'stop':
-        cls = Fonts.stop
-    if style == 'skyline':
-        cls = Fonts.skyline
-    if style == 'arrows':
-        cls = Fonts.arrows
-    if style == 'qvnes':
-        cls = Fonts.rvnes
-    if style == 'strike':
-        cls = Fonts.strike
-    if style == 'frozen':
-        cls = Fonts.frozen
-
-    r, oldtxt = m.message.reply_to_message.text.split(None, 1) 
-    new_text = cls(oldtxt)            
+            await callback.answer("🔒 No Active Plan", show_alert=True)
+        return
+    
+    # Font generation
+    await callback.answer()
+    cmd, style = callback.data.split('+')
+    
+    # Font styles dictionary
+    fonts = {
+        'typewriter': Fonts.typewriter,
+        'outline': Fonts.outline,
+        'serif': Fonts.serief,
+        'bold_cool': Fonts.bold_cool,
+        'cool': Fonts.cool,
+        'small_cap': Fonts.smallcap,
+        'script': Fonts.script,
+        'script_bolt': Fonts.bold_script,
+        'tiny': Fonts.tiny,
+        'comic': Fonts.comic,
+        'sans': Fonts.san,
+        'slant_sans': Fonts.slant_san,
+        'slant': Fonts.slant,
+        'sim': Fonts.sim,
+        'circles': Fonts.circles,
+        'circle_dark': Fonts.dark_circle,
+        'gothic': Fonts.gothic,
+        'gothic_bolt': Fonts.bold_gothic,
+        'cloud': Fonts.cloud,
+        'happy': Fonts.happy,
+        'sad': Fonts.sad,
+        'special': Fonts.special,
+        'squares': Fonts.square,
+        'squares_bold': Fonts.dark_square,
+        'andalucia': Fonts.andalucia,
+        'manga': Fonts.manga,
+        'stinky': Fonts.stinky,
+        'bubbles': Fonts.bubbles,
+        'underline': Fonts.underline,
+        'ladybug': Fonts.ladybug,
+        'rays': Fonts.rays,
+        'birds': Fonts.birds,
+        'slash': Fonts.slash,
+        'stop': Fonts.stop,
+        'skyline': Fonts.skyline,
+        'arrows': Fonts.arrows,
+        'qvnes': Fonts.rvnes,
+        'strike': Fonts.strike,
+        'frozen': Fonts.frozen,
+        # Premium fonts
+        'glitch': lambda x: ''.join([chr(ord(c) + random.randint(-1,1)) if c.isalpha() else c for c in x]),
+        'rainbow': lambda x: ''.join(['🔴' if i%5==0 else '🟠' if i%5==1 else '🟡' if i%5==2 else '🟢' if i%5==3 else '🔵' for i,c in enumerate(x)] + [x]),
+        'fire': lambda x: '🔥 ' + x + ' 🔥'
+    }
+    
+    # Get original text
     try:
-        await m.message.edit_text(f"`{new_text}`\n\n👆 Click To Copy", reply_markup=m.message.reply_markup)
-    except Exception as e:
-        print(e)
+        original_msg = callback.message.reply_to_message or callback.message
+        text = original_msg.text.split(maxsplit=1)[1] if len(original_msg.text.split()) > 1 else "Sample Text"
+    except:
+        text = "Sample Text"
+    
+    # Generate font
+    if style in fonts:
+        new_text = fonts[style](text)
+    else:
+        new_text = text
+    
+    # Premium badge
+    is_premium = await db.is_premium_user(callback.from_user.id)
+    badge = " 💎" if is_premium else ""
+    
+    # Send result
+    await callback.edit_message_text(
+        f"`{new_text}`{badge}\n\n"
+        f"👆 **Click to Copy**\n"
+        f"💎 **Premium = Exclusive fonts!**",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("🔄 New Style", callback_data="font_back")],
+            [InlineKeyboardButton("💎 Premium", callback_data="plan")]
+        ])
+    )
+
+print("✅ Font plugin loaded with Premium!")
