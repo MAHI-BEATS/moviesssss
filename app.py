bf77f08a9ced40b2f1262a1012a673a226fd5d57
SHIV <shivashish4444@gmail.com>
2026-05-12 11:23:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Jisshu bots"
+    
+    # Load plugins
+from plugins import load_plugins
+plugins = load_plugins(app)
+
+# Start premium checker
+import asyncio
+from webapp import check_expired_premium
+asyncio.create_task(check_expired_premium(app))
